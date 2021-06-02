6a00518e86143e26c5994b66befe0b64a0ea6809
JxshCodes <77446872+JxshCodes@users.noreply.github.com>
2021-06-02 19:07:53 -0400

    main

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODQ4NjMzNjMxMDY0NTg4MzI5.YLPdqA.K7ocEaor4_ZGckckem40lVhMrEA',
+        prefix: '%',
+        activity: 'Created By Jxsh 999#0001',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
